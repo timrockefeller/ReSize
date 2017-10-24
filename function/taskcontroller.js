a61bdd8b30f63491d62a5da44b780cbd9aa4bff4
kitekii <tim720719@163.com>
2017-10-24 21:18:07 +0800

    Add Controller Buttons
    with so fukin much v-if

--- function/taskcontroller.js
+++ function/taskcontroller.js
@@ -5,28 +5,57 @@ global Vue
 var ctrl = new Vue({
     el:"#tskctrl",
     data:{
-        isSeen:true,
+        _isSeen:false,
+        set isSeen(val){
+            if(!val){
+                document.getElementById("tskctrl").classList.add("ctrlHide");
+            }else{
+                document.getElementById("tskctrl").classList.remove("ctrlHide");
+            }
+            console.info("seted");
+            this._isSeen=val?true:false;
+        },
+        get isSeen(){
+            return this._isSeen;
+        },
         isAdvance:false,
         num:0,
         donenum:0,
-        dots:[],
-        curs:0
-    },
-    method:{
+        dots:[0],
+        curs:0,
+
+        color:"000000",
+        CssHide:"spaceHide",
+        tagDone:{
+            backgrund:"#"+this.color
+        },
+        
         init:function(TaskData){/*
         TaskData={
-            
+            TaskId: string,
+	        num: int,
+	        everyvalue: int,
+	        donenum: int,
+	        color: string
         }
         */
-            
+            this.num = TaskData.num;
+            this.donenum = TaskData.donenum;
+            this.color = TaskData.color;
+            this.dots = [0];
+            for(var i =1 ;i<=this.num;i++){
+                this.dots[i]=i;
+            }
         },
+    },
+    method:{
         doneNext:function(){
             if(this.num<this.donenum){
                 this.num++;
             }
         },
         isDone:function(cur){
-            return cur<=this.curs;
+            return cur<=this.num;
         }
         
     }
